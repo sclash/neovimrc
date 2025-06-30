62f624ac6d0129c9b7c997fa7385c9a13f106486
sclash <andrea.sergi1@gmail.com>
2025-06-30 10:04:33 +0200

    blink.cmp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -170,32 +170,34 @@ require('mason-lspconfig').setup({
 		lsp_zero.default_setup,
 		lua_ls = function()
 			local lua_opts = lsp_zero.nvim_lua_ls()
-			require('lspconfig').lua_ls.setup(lua_opts)
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			-- require('lspconfig').lua_ls.setup(lua_opts)
+			require('lspconfig').lua_ls.setup{ options = lua_opts, capabilities = capabilities }
 		end,
 	}
 })
 
-local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
--- this is the function that loads the extra snippets to luasnip
--- from rafamadriz/friendly-snippets
-require('luasnip.loaders.from_vscode').lazy_load()
-
-cmp.setup({
-	sources = {
-		{ name = 'path' },
-		{ name = 'nvim_lsp' },
-		{ name = 'nvim_lua' },
-		{ name = 'luasnip', keyword_length = 2 },
-		{ name = 'buffer',  keyword_length = 3 },
-	},
-	formatting = lsp_zero.cmp_format(),
-	mapping = cmp.mapping.preset.insert({
-		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-		-- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-		['<Enter>'] = cmp.mapping.confirm({ select = true }),
-		['<C-Space>'] = cmp.mapping.complete(),
-	}),
-})
+-- local cmp = require('cmp')
+-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+--
+-- -- this is the function that loads the extra snippets to luasnip
+-- -- from rafamadriz/friendly-snippets
+-- require('luasnip.loaders.from_vscode').lazy_load()
+--
+-- cmp.setup({
+-- 	sources = {
+-- 		{ name = 'path' },
+-- 		{ name = 'nvim_lsp' },
+-- 		{ name = 'nvim_lua' },
+-- 		{ name = 'luasnip', keyword_length = 2 },
+-- 		{ name = 'buffer',  keyword_length = 3 },
+-- 	},
+-- 	formatting = lsp_zero.cmp_format(),
+-- 	mapping = cmp.mapping.preset.insert({
+-- 		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+-- 		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+-- 		-- ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+-- 		['<Enter>'] = cmp.mapping.confirm({ select = true }),
+-- 		['<C-Space>'] = cmp.mapping.complete(),
+-- 	}),
+-- })
