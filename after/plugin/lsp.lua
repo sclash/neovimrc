e1a6e05a329e42b1e92cf028414197066720fd42
sclash <andrea.sergi1@gmail.com>
2025-11-29 13:45:22 +0100

    setting up nixd - flake parts

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -59,20 +59,26 @@ nvim_lsp.nixd.setup({
 			options = {
 				nixos = {
 					-- THIS WORKS
-					expr = '(builtins.getFlake "/home/asergi/dotfiles/nixos").nixosConfigurations.nixos-os.options',
+					expr =
+					'(builtins.getFlake "/home/asergi/dotfiles/nixos").nixosConfigurations.nixos-os.options',
 					--
 					-- expr = '(builtins.getFlake ("git+file://" + toString  github:sclash/dotfiles?ref=home-manager/nixos)).nixosConfigurations.nixos-os.options',
 					-- expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.nixos-os.options',
-					},
+				},
 				home_manager = {
-					expr = '(builtins.getFlake "/home/asergi/dotfiles/nixos").homeConfigurations."asergi@nixos-os".options',
+					expr =
+					'(builtins.getFlake "/home/asergi/dotfiles/nixos").homeConfigurations."asergi@nixos-os".options',
 					-- expr = '(builtins.getFlake ("git+file://" + toString  github:sclash/dotfiles?ref=home-manager/nixos)).homeConfigurations."asergi@nixos-os".options',
 					-- expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."asergi@nixos-os".options',
-					},
+				},
+				flake_parts = {
+					expr =
+					'let flake = builtins.getFlake ("/home/asergi/dotfiles/nixos"); in flake.debug.options // flake.currentSystem.options',
 				},
 			},
 		},
-	}
+	},
+}
 )
 
 
