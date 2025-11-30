15d4a578496de8d659e7d69b322b52cd83b5d861
sclash <andrea.sergi1@gmail.com>
2025-11-30 13:24:30 +0100

    show hidden files in telescope, exclude executables

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,15 +1,23 @@
-
 require('telescope').setup({
 	defaults = {
 		file_ignore_patterns = {
 			"node_modules",
 			"__pycache__",
-			"venv"
+			"venv",
+			".git",
+			".cache",
+			-- Excludes files that contain no period (e.g., executables, build artifacts)
+			[=[^[^.]+$]=],
 		},
 		find_files = {
-			hidden = true
+			hidden = true,
 		}
-	}
+	},
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+	},
 })
 
 -- require('telescope').load_extension('harpoon')
@@ -24,5 +32,5 @@ vim.keymap.set('n', '<leader>fG', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({search = vim.fn.input("Grep > ")})
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
