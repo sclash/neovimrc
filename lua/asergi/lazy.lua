788ef7c901b7392a3570914873ad938943094da4
sclash <andrea.sergi1@gmail.com>
2026-02-26 11:21:03 +0100

    molten-nvim not working

--- lua/asergi/lazy.lua
+++ lua/asergi/lazy.lua
@@ -25,7 +25,25 @@ require('lazy').setup({
 		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
 		opts = {
 			processor = "magick_cli",
+			backend = "kitty", -- whatever backend you would like to use
+			max_width = 100,
+			max_height = 12,
+			max_height_window_percentage = math.huge,
+			max_width_window_percentage = math.huge,
+			window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+			window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
 		}
+	},
+	{
+		"benlubas/molten-nvim",
+		version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+		dependencies = { "3rd/image.nvim" },
+		build = ":UpdateRemotePlugins",
+		init = function()
+			-- these are examples, not defaults. Please see the readme
+			vim.g.molten_image_provider = "image.nvim"
+			vim.g.molten_output_win_max_height = 20
+		end,
 	},
 	{ 'Vigemus/iron.nvim' },
 	{
@@ -34,9 +52,6 @@ require('lazy').setup({
 			"benlubas/molten-nvim",
 			"3rd/image.nvim"
 		},
-		config = function()
-			require("pyworks").setup() -- See Configuration section for options
-		end,
 		lazy = false,
 		priority = 100,
 	},
