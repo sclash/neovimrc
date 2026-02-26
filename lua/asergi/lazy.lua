0ebdee2d4c849a5ea052b3a8480bd51a58051644
sclash <andrea.sergi1@gmail.com>
2026-02-26 11:21:45 +0100

    deleted molten-nvim images.nvmi pyworks.nvim

--- lua/asergi/lazy.lua
+++ lua/asergi/lazy.lua
@@ -20,41 +20,7 @@ require('lazy').setup({
 			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
 		}
 	},
-	{
-		"3rd/image.nvim",
-		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-		opts = {
-			processor = "magick_cli",
-			backend = "kitty", -- whatever backend you would like to use
-			max_width = 100,
-			max_height = 12,
-			max_height_window_percentage = math.huge,
-			max_width_window_percentage = math.huge,
-			window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
-			window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-		}
-	},
-	{
-		"benlubas/molten-nvim",
-		version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
-		dependencies = { "3rd/image.nvim" },
-		build = ":UpdateRemotePlugins",
-		init = function()
-			-- these are examples, not defaults. Please see the readme
-			vim.g.molten_image_provider = "image.nvim"
-			vim.g.molten_output_win_max_height = 20
-		end,
-	},
 	{ 'Vigemus/iron.nvim' },
-	{
-		"jeryldev/pyworks.nvim",
-		dependencies = {
-			"benlubas/molten-nvim",
-			"3rd/image.nvim"
-		},
-		lazy = false,
-		priority = 100,
-	},
 	{ 'brenoprata10/nvim-highlight-colors' },
 	{ 'sindrets/diffview.nvim' },
 	{ "lewis6991/gitsigns.nvim" },
